4914f706f0aa895adc536f55907bf152879323c3
Clayton Varnon <claytonvarnon@ymail.com>
2018-03-04 01:30:19 -0600

    added some comments and commented out unused code in publisherGateway

--- enterprise/src/db/PublisherGateway.java
+++ enterprise/src/db/PublisherGateway.java
@@ -1,7 +1,6 @@
 package db;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,14 +8,13 @@ import java.sql.SQLException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import book.Book;
 import utils.AppException;
 import publisher.Publisher;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class PublisherGateway {
-	private static Logger logger = LogManager.getLogger();		//Logger
+	//private static Logger logger = LogManager.getLogger();		//Logger
 	private Connection conn;
 
 	public PublisherGateway (Connection conn) {

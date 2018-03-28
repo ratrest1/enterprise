f60d863ece46615d677105a872e6d6c4b5df7067
Clayton Varnon <claytonvarnon@ymail.com>
2018-03-28 13:50:56 -0500

    Added audit trail code for author gateway; not fully implemented yet.

--- enterprise/src/db/AuthorGateway.java
+++ enterprise/src/db/AuthorGateway.java
@@ -9,7 +9,9 @@ import java.sql.SQLException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import audit.AuditTrailEntry;
 import author.Author;
+import book.Book;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import utils.AppException;
@@ -146,4 +148,158 @@ public class AuthorGateway extends GatewayBase{
 		Author tmp = (Author) arg;
 		deleteAuthor(tmp);
 	}
+	
+	// -------------------------------------------NEW STUFF---------------------------------------------------- //
+	/**
+	 *    createEntry : This method creates an entry when CreateAuthor is called
+	 */
+	private void createEntry (String fname, String lname) throws AppException {
+		PreparedStatement st = null;
+		// get author's id
+		try {
+			st = conn.prepareStatement("select id from author where first_name = ? and last_name = ?");
+			st.setString(1, fname);
+			st.setString(2, lname);
+			ResultSet rs = st.executeQuery();
+			
+			while (rs.next()) {
+				Author author = new Author();
+				author.setId(rs.getInt("id"));
+				createAuditTrailEntry(author.getId(), "Author Created.");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new AppException(e);
+		} finally {
+			try {
+				if(st != null)
+					st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new AppException(e);
+			}
+		}
+	}
+	
+	/**
+	 * updateEntry : This method creates an entry when UpdateBook is called
+	 */
+	private void updateEntry (Author oAuthor) throws AppException {
+		Author nAuthor = new Author();
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("select * from author where id = ?");
+			st.setInt(1, oAuthor.getId());
+			ResultSet rs = st.executeQuery();
+			
+			while (rs.next()) {
+				nAuthor.setId(rs.getInt("id"));
+				nAuthor.setfName(rs.getString("first_name"));
+				nAuthor.setlName(rs.getString("last_name"));
+				nAuthor.setDateOfBirth(rs.getDate("dob").toLocalDate());
+				nAuthor.setGender(rs.getString("gender"));
+				nAuthor.setWebsite(rs.getString("website"));
+			}
+			
+			if ( !nAuthor.getfName().equals(oAuthor.getfName()) ) {
+				createAuditTrailEntry(nAuthor.getId(), "First name changed from " 
+				+ oAuthor.getfName() + " to " + nAuthor.getfName());
+			}
+				
+			if ( !nAuthor.getlName().equals(oAuthor.getlName()) ) {
+				createAuditTrailEntry(nAuthor.getId(), "Last name changed from " 
+				+ oAuthor.getfName() + " to " + nAuthor.getfName());
+			}
+			
+			if (nAuthor.getDateOfBirth() != oAuthor.getDateOfBirth()) {
+				createAuditTrailEntry(nAuthor.getId(), "dob changed from " 
+				+ oAuthor.getDateOfBirth() + " to " + nAuthor.getDateOfBirth());
+			}
+			
+			if ( !nAuthor.getGender().equals(oAuthor.getGender()) ) {
+				createAuditTrailEntry(nAuthor.getId(), "Gender changed from " 
+				+ oAuthor.getGender() + " to " + nAuthor.getGender());
+			}
+			
+			if ( !nAuthor.getWebsite().equals(oAuthor.getWebsite()) ) {
+				createAuditTrailEntry(nAuthor.getId(), "Website changed from " 
+				+ oAuthor.getWebsite() + " to " + nAuthor.getWebsite());
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new AppException(e);
+		} finally {
+			try {
+				if(st != null)
+					st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new AppException(e);
+			}
+		}
+	}
+	
+	/**
+	 * createAuditTrailEntry : called by the above 2 functions and creates an audit trail entry in the db
+	 */
+	public void createAuditTrailEntry (int authorId, String message) throws AppException {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("insert into author_audit_trail ( author_id, entry_msg ) values ( ?, ? )");
+			st.setInt(1, authorId);
+			st.setString(2, message);
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new AppException(e);
+		} finally {
+			try {
+				if(st != null)
+					st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new AppException(e);
+			}
+		}
+		logger.info("Created Audit Trail Entry.");
+	}
+	
+	public ObservableList<AuditTrailEntry> fetchAuditTrail (int authorId) throws AppException {
+		logger.info("Fetching Audit Trail...");
+		ObservableList<AuditTrailEntry> auditTrail = FXCollections.observableArrayList();
+		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("select * from author_audit_trail order by author_id = ?");
+			st.setInt(1, authorId);
+			
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				AuditTrailEntry entry = new AuditTrailEntry();
+				
+				entry.setId(rs.getInt("author_id"));
+				entry.setDateAdded(rs.getDate("date_added"));
+				entry.setMessage(rs.getString("entry_msg"));
+				
+				auditTrail.add(entry);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new AppException(e);
+		} finally {
+			try {
+				if (st != null)
+					st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new AppException(e);
+			}
+		}
+		logger.info("Got Audit Trail.");
+		return auditTrail;
+	}
+	
 }

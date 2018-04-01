efa8ae7fea6b36e1caf70caa669db7d88167eda5
Phish Bait <Phish Bait@LAPTOP-G8M2610B>
2018-04-01 15:00:29 -0500

    Fixed error

--- enterprise/src/authorBook/AuthorBook.java
+++ enterprise/src/authorBook/AuthorBook.java
@@ -24,7 +24,7 @@ public class AuthorBook {
 	public void setMyBook(Book myBook) {
 		this.myBook = myBook;
 	}
-	public int getRoyalty() {
+	public BigDecimal getRoyalty() {
 		return royalty;
 	}
 	public void setRoyalty(int royalty) {
